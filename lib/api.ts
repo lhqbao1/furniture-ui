b30d595a6257af6c7f85e1dacfcd1f7250e12b77
Bao <luonghoangquocbao@gmail.com>
2025-09-08 16:43:18 +0700

    update env

--- lib/api.ts
+++ lib/api.ts
@@ -1,15 +1,11 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://20.61.3.4:8000", // backend của bạn
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`, // backend của bạn
   headers: {
     "Content-Type": "application/json",
   },
 })
-
-
-// lib/api.ts
-import { CartItem } from "@/types/cart"
 
 export async function getCheckOutByCheckOutId(checkoutId: string) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/checkout/${checkoutId}`, {
