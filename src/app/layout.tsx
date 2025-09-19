28970ddf3496557abca7bb0ff0a0da06f4cf3443
Bao <luonghoangquocbao@gmail.com>
2025-09-19 17:27:41 +0700

    fix script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,9 @@ export default function RootLayout({
     return (
         <html lang="de">
             <head>
+                <Script id="GTM" strategy="beforeInteractive">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WKVQP2QH');`}
+                </Script>
                 <Script
                     id="usercentrics-autoblocker"
                     src="https://web.cmp.usercentrics.eu/modules/autoblocker.js"
@@ -49,9 +52,6 @@ export default function RootLayout({
                     async
                     strategy="afterInteractive"
                 />
-                <Script id="GTM" strategy="beforeInteractive">
-                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WKVQP2QH');`}
-                </Script>
             </head>
             <body className={`${figtree.variable} ${libre.variable} font-sans antialiased`}>
                 {/* Google Tag Manager (noscript) */}
