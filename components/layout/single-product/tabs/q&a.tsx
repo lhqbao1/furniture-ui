3fcc89ca883cc468b3af05411334aa0a949b5e9a
Bao <luonghoangquocbao@gmail.com>
2025-10-17 17:34:31 +0700

    update table tiptap

--- components/layout/single-product/tabs/q&a.tsx
+++ components/layout/single-product/tabs/q&a.tsx
@@ -7,7 +7,7 @@ const QASection = () => {
     return (
         <div className='grid grid-cols-12 gap-8 xl:py-4 pt-0'>
             <div className='col-span-7 flex flex-col gap-6 py-2'>
-                <QAInput />
+                {/* <QAInput /> */}
                 {/* <QAAccordion /> */}
             </div>
             {/* <div className='col-span-5 flex flex-col gap-6'>
