a67a9ad3e2172a80f8ddabe8f4d0eb9e75526d14
Bao <lhqbao1@gmail.com>
2025-11-05 18:35:42 +0700

    update 0511

--- components/layout/auth/signup-form.tsx
+++ components/layout/auth/signup-form.tsx
@@ -1,44 +1,47 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { z } from "zod"
-import "react-phone-input-2/lib/style.css"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import Image from "next/image"
-import { useCheckMailExist, useSignUp } from "@/features/auth/hook"
-import { toast } from "sonner"
-import { Loader2 } from "lucide-react"
-import { useRouter } from "@/src/i18n/navigation"
-import { useLocale, useTranslations } from "next-intl"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import "react-phone-input-2/lib/style.css";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useCheckMailExist, useSignUp } from "@/features/auth/hook";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "@/src/i18n/navigation";
+import { useLocale, useTranslations } from "next-intl";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 export default function SignUpForm() {
-  const t = useTranslations()
-  const signUp = useSignUp()
-  const checkMailMutation = useCheckMailExist()
-  const router = useRouter()
-  const locale = useLocale()
+  const t = useTranslations();
+  const signUp = useSignUp();
+  const checkMailMutation = useCheckMailExist();
+  const router = useRouter();
+  const locale = useLocale();
 
   const formSchema = z.object({
-    email: z
-      .string()
-      .min(1, t('emailRequired'))
-      .email(t('invalidEmail')),
-    first_name: z.string().min(1, { message: t('first_name_required') }),
-    last_name: z.string().min(1, { message: t('last_name_required') }),
+    email: z.string().min(1, t("emailRequired")).email(t("invalidEmail")),
+    first_name: z.string().min(1, { message: t("first_name_required") }),
+    last_name: z.string().min(1, { message: t("last_name_required") }),
     phone_number: z
       .string()
-      .min(6, { message: t('phone_number_short') })
+      .min(6, { message: t("phone_number_short") })
       .refine((val) => /^\+?[0-9]+$/.test(val), {
-        message: t('phone_number_invalid'),
+        message: t("phone_number_invalid"),
       }),
-    gender: z.string().min(1, { message: t('gender_required') })
-  })
+    gender: z.string().min(1, { message: t("gender_required") }),
+  });
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -47,51 +50,50 @@ export default function SignUpForm() {
       first_name: "",
       last_name: "",
       phone_number: "",
-      gender: ""
+      gender: "",
     },
     mode: "onSubmit",
-  })
+  });
 
   const handleSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values)
     checkMailMutation.mutate(values.email, {
       onSuccess(data, variables, context) {
-        signUp.mutate({
-          email: values.email,
-          phone_number: values.phone_number,
-          first_name: values.first_name,
-          last_name: values.last_name,
-          gender: values.gender
-        }, {
-          onSuccess: (data) => {
-            form.reset()
-            toast.success(t('signUpSuccess'))
-            router.push("/login", { locale })
+        signUp.mutate(
+          {
+            email: values.email,
+            phone_number: values.phone_number,
+            first_name: values.first_name,
+            last_name: values.last_name,
+            gender: values.gender,
           },
-          onError: (error) => {
-            toast.error(t('signUpFail'))
+          {
+            onSuccess: (data) => {
+              form.reset();
+              toast.success(t("signUpSuccess"));
+              router.push("/login", { locale });
+            },
+            onError: (error) => {
+              toast.error(t("signUpFail"));
+            },
           }
-        })
+        );
       },
       onError(error, variables, context) {
-        toast.error(t('useDifferentEmail'))
+        toast.error(t("useDifferentEmail"));
       },
-    })
-  }
+    });
+  };
 
   return (
     <div className="w-full p-6">
       <div className="flex flex-col items-center mb-12 gap-3">
         {/* Logo giả */}
-        <Image
-          src={'/new-logo.svg'}
-          width={100}
-          height={100}
-          alt=""
-        />
+        <Image src={"/new-logo.svg"} width={100} height={100} alt="" />
         <h1 className="text-3xl font-semibold text-secondary text-center space-x-2 lg:block flex flex-col">
-          <span>{t('welcomeTo')}</span>
-          <span className="text-primary" translate="no">Prestige Home</span>
+          <span>{t("welcomeTo")}</span>
+          <span className="text-primary" translate="no">
+            Prestige Home
+          </span>
         </h1>
       </div>
 
@@ -99,13 +101,15 @@ export default function SignUpForm() {
         <form
           onSubmit={form.handleSubmit(
             (values) => {
-              handleSubmit(values)
+              handleSubmit(values);
             },
             (errors) => {
-              console.log(errors)
-              toast.error("Please check the form for errors")
+              console.log(errors);
+              toast.error("Please check the form for errors");
             }
-          )} className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          )}
+          className="grid grid-cols-1 md:grid-cols-2 gap-6"
+        >
           <div className="md:col-span-2 col-span-1">
             <FormField
               control={form.control}
@@ -114,7 +118,11 @@ export default function SignUpForm() {
                 <FormItem>
                   {/* <FormLabel>{t("gender")}</FormLabel> */}
                   <FormControl>
-                    <RadioGroup onValueChange={field.onChange} value={field.value} className="flex gap-4">
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      className="flex gap-4"
+                    >
                       <FormItem className="flex gap-1 items-center">
                         <FormControl>
                           <RadioGroupItem value="male" />
@@ -131,7 +139,9 @@ export default function SignUpForm() {
                         <FormControl>
                           <RadioGroupItem value="other" />
                         </FormControl>
-                        <FormLabel className="ml-2">{t("otherGender")}</FormLabel>
+                        <FormLabel className="ml-2">
+                          {t("otherGender")}
+                        </FormLabel>
                       </FormItem>
                     </RadioGroup>
                   </FormControl>
@@ -145,7 +155,7 @@ export default function SignUpForm() {
             name="first_name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="">{t('first_name')}</FormLabel>
+                <FormLabel className="">{t("first_name")}</FormLabel>
                 <FormControl>
                   <Input placeholder="" {...field} />
                 </FormControl>
@@ -158,7 +168,7 @@ export default function SignUpForm() {
             name="last_name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>{t('last_name')}</FormLabel>
+                <FormLabel>{t("last_name")}</FormLabel>
                 <FormControl>
                   <Input placeholder="" {...field} />
                 </FormControl>
@@ -171,7 +181,7 @@ export default function SignUpForm() {
             name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>{t('email')}</FormLabel>
+                <FormLabel>{t("email")}</FormLabel>
                 <FormControl>
                   <Input placeholder="" {...field} />
                 </FormControl>
@@ -184,13 +194,9 @@ export default function SignUpForm() {
             name="phone_number"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>{t('phone_number')}</FormLabel>
+                <FormLabel>{t("phone_number")}</FormLabel>
                 <FormControl>
-                  <Input
-                    type="number"
-                    placeholder="+49"
-                    {...field}
-                  />
+                  <Input type="number" placeholder="+49" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -198,14 +204,20 @@ export default function SignUpForm() {
           />
 
           <div className="col-span-1 md:col-span-2 flex justify-center mt-4">
-            <Button type="submit" className="bg-primary/90 hover:bg-primary lg:px-12 px-4 py-6 text-lg" hasEffect>
+            <Button
+              type="submit"
+              className="bg-primary/90 hover:bg-primary lg:px-12 px-4 py-6 text-lg"
+              hasEffect
+            >
               {signUp.isPending ? (
                 <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-              ) : <div className="capitalize">{t('createAccount')}</div>}
+              ) : (
+                <div className="capitalize">{t("createAccount")}</div>
+              )}
             </Button>
           </div>
         </form>
       </Form>
     </div>
-  )
+  );
 }
