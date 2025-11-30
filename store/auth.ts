85844e72e8f12294563a5b0bf94927338eac7192
Bao <lhqbao1@gmail.com>
2025-11-30 16:53:46 +0700

    update

--- store/auth.ts
+++ store/auth.ts
@@ -2,6 +2,10 @@
 import { atomWithStorage } from "jotai/utils";
 
 export const userIdAtom = atomWithStorage<string | null>("user_id", null);
+export const adminIdAtom = atomWithStorage<string | null>(
+  "admin_user_id",
+  null,
+);
 
 export const accessTokenAtom = atomWithStorage<string | null>(
   "access_token",
