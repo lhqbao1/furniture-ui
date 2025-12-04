63afd991f80fbb756307139762e6063e2f706248
Bao <lhqbao1@gmail.com>
2025-12-04 18:19:17 +0700

    update whatsapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,6 +94,33 @@ export default function RootLayout({
           strategy="afterInteractive"
           defer
         />
+
+        <Script
+          id="whatsapp-widget"
+          strategy="afterInteractive"
+        >
+          {`
+          (function () {
+            var options = {
+              whatsapp: "+84889835259",
+              call_to_action: "Chatten Sie mit uns!",
+              position: "right",
+            };
+            var proto = document.location.protocol,
+              host = "getbutton.io",
+              url = proto + "//static." + host;
+            var s = document.createElement("script");
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = url + "/widget-send-button/js/init.js";
+            s.onload = function () {
+              WhWidgetSendButton.init(host, proto, options);
+            };
+            var x = document.getElementsByTagName("script")[0];
+            x.parentNode.insertBefore(s, x);
+          })();
+        `}
+        </Script>
       </head>
 
       <body
