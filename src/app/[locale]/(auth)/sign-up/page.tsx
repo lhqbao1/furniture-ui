ba660930de559938799e122f26f983432104a8c9
Bao <lhqbao1@gmail.com>
2025-12-12 00:26:41 +0700

    update

--- src/app/[locale]/(auth)/sign-up/page.tsx
+++ src/app/[locale]/(auth)/sign-up/page.tsx
@@ -2,6 +2,24 @@ import SignUpForm from "@/components/layout/auth/signup-form";
 import Image from "next/image";
 import React from "react";
 
+export const metadata = {
+  title: "Registrierung",
+  description:
+    "Erstellen Sie Ihr Prestige Home Konto, um Bestellungen zu verwalten, exklusive Angebote zu erhalten und Ihre persönlichen Einstellungen zu speichern.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+  openGraph: {
+    title: "Registrierung",
+    description:
+      "Registrieren Sie sich, um auf Ihr Prestige Home Konto zuzugreifen und personalisierte Funktionen zu nutzen.",
+    url: "/sign-up",
+    siteName: "Prestige Home",
+    type: "website",
+  },
+};
+
 const SignUpPage = () => {
   return (
     <div className="grid grid-cols-12 w-screen h-screen">
