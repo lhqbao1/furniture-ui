db25217ff8f58107eb9c5d8d72d28ea7628889d7
Bao <lhqbao1@gmail.com>
2025-12-30 12:21:20 +0700

    update

--- components/layout/single-product/tabs/description/faq-accordion.tsx
+++ components/layout/single-product/tabs/description/faq-accordion.tsx
@@ -11,6 +11,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { useEffect, useState } from "react";
 import { TypingLoader } from "./typing-dot";
+import { useTranslations } from "next-intl";
 
 interface ProductFAQProps {
   question: ProductFAQ[];
@@ -18,13 +19,14 @@ interface ProductFAQProps {
 
 export function ProductFAQSection({ question }: ProductFAQProps) {
   const [loadingKey, setLoadingKey] = useState<string | null>(null);
+  const t = useTranslations();
 
   if (!question?.length) return null;
 
   return (
     <>
       <div className="text-2xl text-primary font-semibold">
-        Check with Real AI:{" "}
+        {t("checkWithAI")}:{" "}
         <span className="text-3xl text-black font-medium">Chat GPT</span>
       </div>
       <Accordion
@@ -47,7 +49,7 @@ export function ProductFAQSection({ question }: ProductFAQProps) {
             <AccordionItem
               value={item.question}
               key={item.question}
-              className="border rounded-sm px-2"
+              className="border rounded-sm px-4 py-4"
             >
               <AccordionTrigger className="py-2 flex gap-4 items-center">
                 <p className="text-xl text-secondary font-bold">Q</p>
@@ -55,7 +57,7 @@ export function ProductFAQSection({ question }: ProductFAQProps) {
               </AccordionTrigger>
 
               <AccordionContent>
-                <div className="flex gap-4 pt-3">
+                <div className="flex gap-4 px-3 py-4 border rounded-xl">
                   <p className="text-xl text-primary font-bold">A</p>
 
                   {isLoading ? (
