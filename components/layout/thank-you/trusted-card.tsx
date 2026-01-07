b020699b6807f457867fd0fd1d543368d08dda95
Bao <lhqbao1@gmail.com>
2026-01-07 10:24:07 +0700

    update

--- components/layout/thank-you/trusted-card.tsx
+++ components/layout/thank-you/trusted-card.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { mapTrustedShopsPaymentType } from "@/hooks/map-payment-method";
 import { ProductItem } from "@/types/products";
+import Script from "next/script";
 
 interface TrustedShopsCheckoutProps {
   orderNumber: string;
@@ -12,49 +14,62 @@ interface TrustedShopsCheckoutProps {
   products: ProductItem[];
 }
 
-export function TrustedShopsCheckout({
-  orderNumber,
-  buyerEmail,
-  amount,
-  currency,
-  paymentType,
-  estimatedDeliveryDate,
-  products,
-}: TrustedShopsCheckoutProps) {
-  return (
-    <div
-      id="trustedShopsCheckout"
-      style={{ display: "none" }} // ❗ BẮT BUỘC
-    >
-      <span id="tsCheckoutOrderNr">{orderNumber}</span>
-      <span id="tsCheckoutBuyerEmail">{buyerEmail}</span>
-      <span id="tsCheckoutOrderAmount">{amount.toFixed(2)}</span>
-      <span id="tsCheckoutOrderCurrency">{currency}</span>
-      <span id="tsCheckoutOrderPaymentType">{paymentType}</span>
-      <span id="tsCheckoutOrderEstDeliveryDate">{estimatedDeliveryDate}</span>
+export function TrustedShopsCheckout(props: TrustedShopsCheckoutProps) {
+  const {
+    orderNumber,
+    buyerEmail,
+    amount,
+    currency,
+    paymentType,
+    estimatedDeliveryDate,
+    products,
+  } = props;
 
-      {/* 🔹 Product reviews */}
-      {products.map((product, index) => (
-        <span
-          className="tsCheckoutProductItem"
-          key={index}
-        >
-          <span className="tsCheckoutProductUrl">{`https://prestige-home.de/de/product/${product.url_key}`}</span>
-          <span className="tsCheckoutProductImageUrl">
-            {product.static_files[0].url}
-          </span>
-          <span className="tsCheckoutProductName">{product.name}</span>
-          <span className="tsCheckoutProductSKU">{product.sku}</span>
+  const html = `
+    <div id="trustedShopsCheckout" style="display:none">
+      <span id="tsCheckoutOrderNr">${orderNumber}</span>
+      <span id="tsCheckoutBuyerEmail">${buyerEmail}</span>
+      <span id="tsCheckoutOrderAmount">${amount.toFixed(2)}</span>
+      <span id="tsCheckoutOrderCurrency">${currency}</span>
+      <span id="tsCheckoutOrderPaymentType">
+        ${mapTrustedShopsPaymentType(paymentType)}
+      </span>
+      <span id="tsCheckoutOrderEstDeliveryDate">${estimatedDeliveryDate}</span>
 
-          {product.ean && (
-            <span className="tsCheckoutProductGTIN">{product.ean}</span>
-          )}
-
-          {product.brand && (
-            <span className="tsCheckoutProductBrand">{product.brand.name}</span>
-          )}
+      ${products
+        .map(
+          (p) => `
+        <span class="tsCheckoutProductItem">
+          <span class="tsCheckoutProductUrl">https://prestige-home.de/de/product/${
+            p.url_key
+          }</span>
+          <span class="tsCheckoutProductImageUrl">${
+            p.static_files?.[0]?.url ?? ""
+          }</span>
+          <span class="tsCheckoutProductName">${p.name}</span>
+          <span class="tsCheckoutProductSKU">${p.sku}</span>
+          ${p.ean ? `<span class="tsCheckoutProductGTIN">${p.ean}</span>` : ""}
+          ${
+            p.brand?.name
+              ? `<span class="tsCheckoutProductBrand">${p.brand.name}</span>`
+              : ""
+          }
         </span>
-      ))}
+      `,
+        )
+        .join("")}
     </div>
+  `;
+
+  return (
+    <>
+      {/* TS script phải load TRƯỚC */}
+      <Script
+        src="https://widgets.trustedshops.com/js/YOUR_TS_ID.js"
+        strategy="beforeInteractive"
+      />
+
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </>
   );
 }
