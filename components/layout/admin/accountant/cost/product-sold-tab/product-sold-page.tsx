f906a7be677b7d59e9570321627f2d17de88048a
Bao <lhqbao1@gmail.com>
2026-01-09 12:26:19 +0700

    update

--- components/layout/admin/accountant/cost/product-sold-tab/product-sold-page.tsx
+++ components/layout/admin/accountant/cost/product-sold-tab/product-sold-page.tsx
@@ -7,7 +7,6 @@ import { DashboardStats } from "./stat";
 import { DashboardStatsSkeleton } from "./stats-skeleton";
 import { ProviderDrawer } from "./product-drawer";
 import { ProductSoldHeader } from "./product-sold-header";
-import { useSearchParams } from "next/navigation";
 import { formatLocal, getMonthRange } from "@/hooks/get-month-range-by-month";
 
 const ProductSoldPage = () => {
@@ -30,27 +29,6 @@ const ProductSoldPage = () => {
     to_date,
   });
 
-  // const [open, setOpen] = useState(false);
-  // const [selectedProviderId, setSelectedProviderId] = useState<string | null>(
-  //   null,
-  // );
-  // const searchParams = useSearchParams();
-
-  // const fromDateParam = searchParams.get("from_date") ?? undefined;
-
-  // const selectedMonth = React.useMemo(() => {
-  //   if (!fromDateParam) return null;
-  //   return new Date(fromDateParam);
-  // }, [fromDateParam]);
-
-  // const currentRange = React.useMemo(() => {
-  //   if (!selectedMonth) return undefined;
-  //   return getMonthRange(selectedMonth);
-  // }, [selectedMonth]);
-
-  // const { data, isLoading } = useGetProductsCheckOutDashboard(currentRange);
-
-  console.log(data);
   return (
     <div className="space-y-7">
       <ProductSoldHeader
