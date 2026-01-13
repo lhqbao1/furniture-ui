618a32f6c6b230e417f18e17c902ed3c8df56592
Bao <lhqbao1@gmail.com>
2026-01-13 14:28:18 +0700

    update

--- components/layout/category/category-page.tsx
+++ components/layout/category/category-page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import CustomBreadCrumb from "@/components/shared/breadcrumb";
 import ProductsGridLayout from "@/components/shared/products-grid-layout";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ProductGridSkeleton } from "@/components/shared/product-grid-skeleton";
 import { CustomPagination } from "@/components/shared/custom-pagination";
 import { useAtom } from "jotai";
@@ -13,6 +13,10 @@ import { useTranslations } from "next-intl";
 import { CategoryBySlugResponse } from "@/types/categories";
 import { useQuery } from "@tanstack/react-query";
 import { getCategoryBySlug } from "@/features/category/api";
+import ShopAllFilterSection from "../shop-all/shop-all-filter-section";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "@/src/i18n/navigation";
+import { useProductsAlgoliaSearch } from "@/features/products/hook";
 
 interface ProductCategoryProps {
   categorySlugs: string[];
@@ -26,11 +30,40 @@ const ProductCategory = ({
   category,
 }: ProductCategoryProps) => {
   const t = useTranslations();
-  const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(16);
-  const [currentCategoryName, setCurrentCategoryName] = useAtom(
-    currentCategoryNameAtom,
-  );
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  // 🔹 1. LẤY PARAMS TỪ URL
+  const query = searchParams.get("search") ?? undefined;
+  const pageFromUrl = Number(searchParams.get("page")) || 1;
+  const pageSizeFromUrl = Number(searchParams.get("page_size")) || 40;
+
+  const brands = searchParams.getAll("brand");
+  const brandsKey =
+    brands.length > 0 ? brands.slice().sort().join("|") : undefined;
+
+  const colors = searchParams.getAll("color");
+  const colorsKey =
+    colors.length > 0 ? colors.slice().sort().join("|") : undefined;
+
+  const materials = searchParams.getAll("materials");
+  const materialsKey =
+    materials.length > 0 ? materials.slice().sort().join("|") : undefined;
+
+  const deliveryTime = searchParams.getAll("delivery_time");
+  const deliveryTimeKey =
+    deliveryTime.length > 0 ? deliveryTime.slice().sort().join("|") : undefined;
+
+  // 🔹 2. STATE (sync với URL)
+  const [page, setPage] = useState(pageFromUrl);
+  const [pageSize, setPageSize] = useState(pageSizeFromUrl);
+
+  // 🔹 3. SYNC khi back / reload
+  useEffect(() => {
+    setPage(pageFromUrl);
+    setPageSize(pageSizeFromUrl);
+  }, [pageFromUrl, pageSizeFromUrl]);
+
   const { data: categoryData, isFetching } = useQuery({
     queryKey: ["category", categorySlugs, page, pageSize],
     queryFn: () =>
@@ -46,39 +79,78 @@ const ProductCategory = ({
     refetchOnWindowFocus: false,
   });
 
+  const { data: algoliaData, isFetching: isAlgoliaLoading } =
+    useProductsAlgoliaSearch({
+      page,
+      page_size: pageSize,
+      is_active: true,
+      is_econelo: false,
+      query,
+      brand: brands,
+      brandsKey,
+      color: colors,
+      colorsKey,
+      materials,
+      materialsKey,
+      delivery_time: deliveryTime,
+      delivery_timeKey: deliveryTimeKey,
+      categories: categoryData ? [categoryData.name] : [], // 👈
+      categoriesKey: categoryData?.name, // 👈
+    });
+
+  const handlePageChange = (newPage: number) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", newPage.toString());
+
+    router.push(`/shop-all?${params.toString()}`, { scroll: false });
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className="pt-3 xl:pb-16 pb-6">
-      <CustomBreadCrumb currentPage={category?.name ?? ""} />
-      <div className="">
-        <h2 className="section-header">{category?.name}</h2>
-        {category?.products.length === 0 ? (
-          <p className="text-center text-xl font-bold mt-2">
-            {t("emptyCategory")}
-          </p>
-        ) : (
-          ""
-        )}
-
-        {!categoryData && isFetching ? (
-          <ProductGridSkeleton length={12} />
-        ) : (
-          <div className="filter-section">
-            <div className="pt-2 md:pt-6 lg:pt-10 pb-12">
-              <ProductsGridLayout
-                hasBadge
-                data={categoryData?.products ?? []}
-              />
-            </div>
+      <div className="grid grid-cols-12 gap-6">
+        <div className="col-span-2 md:block md:col-span-4 lg:col-span-3 xl:col-span-2 hidden">
+          <div className="sticky top-48 max-h-[calc(100vh-6rem)] pr-2 overflow-y-auto no-scrollbar">
+            <ShopAllFilterSection isShopAll={false} />
           </div>
-        )}
+        </div>
+        <div className="pt-0 pb-12 lg:w-[90%] md:w-[95%] xl:w-[90%] w-full mx-auto col-span-12 md:col-span-8 lg:col-span-9 xl:col-span-10">
+          <CustomBreadCrumb currentPage={category?.name ?? ""} />
+          <h2 className="section-header">{category?.name}</h2>
+          {category?.products.length === 0 ? (
+            <p className="text-center text-xl font-bold mt-2">
+              {t("emptyCategory")}
+            </p>
+          ) : (
+            ""
+          )}
+
+          {!categoryData && !algoliaData && isFetching && isAlgoliaLoading ? (
+            <ProductGridSkeleton length={12} />
+          ) : (
+            <div className="filter-section">
+              <div className="pt-2 md:pt-6 lg:pt-10 pb-12">
+                {algoliaData && algoliaData?.items.length > 0 ? (
+                  <ProductsGridLayout
+                    hasBadge
+                    data={algoliaData?.items ?? []}
+                  />
+                ) : (
+                  <div className="text-center">{t("noProducts")}</div>
+                )}
+              </div>
+            </div>
+          )}
+          {algoliaData && algoliaData.pagination.total_items > 16 && (
+            <CustomPagination
+              totalPages={algoliaData.pagination.total_pages}
+              page={page}
+              onPageChange={handlePageChange}
+            />
+          )}
+        </div>
       </div>
-      {categoryData && categoryData.total_items > 16 && (
-        <CustomPagination
-          totalPages={categoryData.total_pages}
-          page={page}
-          onPageChange={setPage}
-        />
-      )}
     </div>
   );
 };
