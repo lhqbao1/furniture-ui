c869a5a41f4b929b4831863550c902add52ed8c0
Bao <lhqbao1@gmail.com>
2026-01-14 14:57:08 +0700

    update gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,6 +85,19 @@ export default function RootLayout({
             __html: `window.__APP_VERSION__ = "${process.env.NEXT_PUBLIC_APP_VERSION}";`,
           }}
         />
+
+        <Script
+          id="gtm-head"
+          strategy="beforeInteractive"
+        >
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=GTM-WSBWTB5'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WSBWTB5');
+          `}
+        </Script>
         {/* <meta
           name="apple-mobile-web-app-capable"
           content="no"
@@ -266,11 +279,11 @@ export default function RootLayout({
         <Providers>
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-WKVQP2QH"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WSBWTB5"
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
+            />
           </noscript>
 
           <noscript>
