47f873326b9ba42b0c2bd8567ba7a230c1b719d5
Bao <lhqbao1@gmail.com>
2026-01-14 15:05:11 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,8 +94,8 @@ export default function RootLayout({
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=GTM-WSBWTB5'+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WSBWTB5');
+            'https://www.googletagmanager.com/gtm.js?id=GTM-WSBWFTB5'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WSBWFTB5');
           `}
         </Script>
         {/* <meta
@@ -279,7 +279,7 @@ export default function RootLayout({
         <Providers>
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-WSBWTB5"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WSBWFTB5"
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
