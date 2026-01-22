8c326b13fa1843750397535b4a351c6134719acd
Bao <lhqbao1@gmail.com>
2026-01-22 18:48:29 +0700

    update

---
+++ components/layout/admin/incoming-inventory/seller-banking-information.tsx
@@ -0,0 +1,118 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import React from "react";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { useFormContext } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+
+const SellerBankingInformation = () => {
+  const { control } = useFormContext();
+
+  return (
+    <Card className="col-span-4">
+      <CardHeader className="text-xl text-secondary font-semibold">
+        Bank Information
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-cols-2 gap-4">
+          {/* ===== Bank Name ===== */}
+          <FormField
+            control={control}
+            name="bank_name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bank Name</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    value={field.value ?? ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* ===== Bank Address ===== */}
+          <FormField
+            control={control}
+            name="bank_address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bank Address</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    value={field.value ?? ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* ===== Bank City ===== */}
+          <FormField
+            control={control}
+            name="bank_account_no"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bank City</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    value={field.value ?? ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* ===== Bank Country ===== */}
+          <FormField
+            control={control}
+            name="bank_swift"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bank Country</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    value={field.value ?? ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* ===== Bank Postal Code ===== */}
+          <FormField
+            control={control}
+            name="bank_currency"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bank Postal Code</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    value={field.value ?? ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SellerBankingInformation;
--- src/app/[locale]/(admin)/admin/logistic/inventory/incoming/add/page.tsx
+++ src/app/[locale]/(admin)/admin/logistic/inventory/incoming/add/page.tsx
@@ -1,9 +1,11 @@
+import AddIncomingInventoryForm from "@/components/layout/admin/incoming-inventory/add-incoming-inventory-form";
 import React from "react";
 
 const CreateIncomingInventory = () => {
   return (
-    <div className="space-y-6">
-      <div className="grid grid-cols-3"></div>
+    <div className="space-y-6 mt-6">
+      <AddIncomingInventoryForm />
+      <div>List products with container wrapped</div>
     </div>
   );
 };
