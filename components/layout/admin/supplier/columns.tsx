4315c9158e624f076e2b538746722ffe017a12ca
Bao <lhqbao1@gmail.com>
2026-01-29 11:37:29 +0700

    update

--- components/layout/admin/supplier/columns.tsx
+++ components/layout/admin/supplier/columns.tsx
@@ -1,7 +1,13 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { ColumnDef } from "@tanstack/react-table";
 import { Pencil } from "lucide-react";
 import { useState } from "react";
@@ -10,89 +16,97 @@ import DeleteDialog from "./delete-dialog";
 import AddOrEditSupplierForm from "./add-or-edit-form";
 
 const EditSupplierCell = ({ supplier }: { supplier: SupplierResponse }) => {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-    return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger asChild>
-                <Button variant="ghost" size="icon">
-                    <Pencil className="w-4 h-4 text-primary" />
-                </Button>
-            </DialogTrigger>
-            <DialogContent className="w-1/3">
-                <DialogHeader>
-                    <DialogTitle>Edit Supplier</DialogTitle>
-                </DialogHeader>
-                <AddOrEditSupplierForm supplierValues={supplier} onClose={() => setOpen(false)} />
-            </DialogContent>
-        </Dialog>
-    )
-}
-
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+        >
+          <Pencil className="w-4 h-4 text-primary" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-1/3">
+        <DialogHeader>
+          <DialogTitle>Edit Supplier</DialogTitle>
+        </DialogHeader>
+        <AddOrEditSupplierForm
+          supplierValues={supplier}
+          onClose={() => setOpen(false)}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export const supplierColumns: ColumnDef<SupplierResponse>[] = [
-    {
-        id: "select",
-        header: ({ table }) => (
-            <Checkbox
-                checked={
-                    table.getIsAllPageRowsSelected() ||
-                    (table.getIsSomePageRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                aria-label="Select all"
-            />
-        ),
-        cell: ({ row }) => (
-            <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-    },
-    {
-        accessorKey: "business_name",
-        header: "NAME",
-        cell: ({ row }) => <div>{row.original.business_name}</div>
-    },
-    {
-        accessorKey: "vat_id",
-        header: "VAT",
-        cell: ({ row }) => <div>{row.original.vat_id}</div>
-    },
-    {
-        accessorKey: "email",
-        header: "EMAIl",
-        cell: ({ row }) => <div>{row.original.email}</div>
-    },
-    {
-        accessorKey: "email_order",
-        header: "ORDER EMAIL",
-        cell: ({ row }) => <div>{row.original.email_order}</div>
-    },
-    {
-        accessorKey: "email_billing",
-        header: "BILLING EMAIL",
-        cell: ({ row }) => <div>{row.original.email_billing}</div>
-    },
-    {
-        accessorKey: "phone_number",
-        header: "PHONE NUMBER",
-        cell: ({ row }) => <div>{row.original.phone_number}</div>
-    },
-    {
-        id: "actions",
-        header: "ACTION",
-        cell: ({ row }) => {
-            return (
-                <div className="flex gap-1">
-                    <EditSupplierCell supplier={row.original} />
-                    <DeleteDialog supplier_id={row.original.id} />
-                </div>
-            )
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
         }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "business_name",
+    header: "NAME",
+    cell: ({ row }) => <div>{row.original.business_name}</div>,
+  },
+  {
+    accessorKey: "vat_id",
+    header: "VAT",
+    cell: ({ row }) => <div>{row.original.vat_id}</div>,
+  },
+  {
+    accessorKey: "email",
+    header: "EMAIl",
+    cell: ({ row }) => <div>{row.original.email}</div>,
+  },
+  {
+    accessorKey: "email_order",
+    header: "ORDER EMAIL",
+    cell: ({ row }) => <div>{row.original.email_order}</div>,
+  },
+  {
+    accessorKey: "email_billing",
+    header: "BILLING EMAIL",
+    cell: ({ row }) => <div>{row.original.email_billing}</div>,
+  },
+  {
+    accessorKey: "phone_number",
+    header: "PHONE NUMBER",
+    cell: ({ row }) => <div>{row.original.phone_number}</div>,
+  },
+  {
+    id: "actions",
+    header: "ACTION",
+    cell: ({ row }) => {
+      return (
+        <div className="flex gap-1">
+          <EditSupplierCell supplier={row.original} />
+          {/* <DeleteDialog supplier_id={row.original.id} /> */}
+        </div>
+      );
     },
-]
+  },
+];
--- features/incoming-inventory/container/hook.ts
+++ features/incoming-inventory/container/hook.ts
@@ -3,6 +3,7 @@ import { POContainerDetail } from "@/types/po";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   createPOContainer,
+  deletePOContainer,
   getContainersByPurchaseOrder,
   updatePOContainer,
 } from "./api";
@@ -45,5 +46,18 @@ export function useGetContainersByPurchaseOrder(purchaseOrderId: string) {
     queryKey: ["po-containers", purchaseOrderId],
     queryFn: () => getContainersByPurchaseOrder(purchaseOrderId!),
     enabled: !!purchaseOrderId,
+  });
+}
+
+export function useDeletePOContainer() {
+  const queryClient = useQueryClient();
+
+  return useMutation<unknown, Error, { containerId: string }>({
+    mutationFn: ({ containerId }) => deletePOContainer(containerId),
+    onSuccess: (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ["po-containers"],
+      });
+    },
   });
 }
