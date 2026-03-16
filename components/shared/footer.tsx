8e773b17392a9c7e340929d2918e83362d809724
Bao <luonghoangquocbao@gmail.com>
2026-03-16 19:09:33 +0700

    update

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -124,11 +124,19 @@ const Footer = () => {
 
         <div className="grid grid-cols-2 gap-6 items-start">
           <ul className="space-y-2 text-black-700 text-sm">
+            <li className="font-bold">Büro </li>
             <li>Prestige Home GmbH</li>
             <li>Greifswalder Straße 226, 10405 Berlin</li>
-            <li>{t("phone_number")}: 02921 327 20 38</li>
+            <li>{t("phone_number")}: +49 30 814 537 080</li>
             <li>{t("email")}: info@prestige-home.de</li>
             <li>Mo – Fr: 09:00 – 17:00 Uhr</li>
+          </ul>
+          <ul className="space-y-2 text-black-700 text-sm">
+            <li className="font-bold">Lager</li>
+            <li>Prestige Home</li>
+            <li>Amm GmbH & Co KG Spedition</li>
+            <li>Hamburger Straße 99</li>
+            <li>90451 Nuremberg</li>
           </ul>
         </div>
       </div>
