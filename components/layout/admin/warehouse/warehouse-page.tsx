ebee7b443da6191667f5e98aef465a9a94e0434c
Bao <luonghoangquocbao@gmail.com>
2026-09-14 15:13:04 +0700

    update

--- components/layout/admin/warehouse/warehouse-page.tsx
+++ components/layout/admin/warehouse/warehouse-page.tsx
@@ -478,7 +478,7 @@ const buildSpeditionPayload = (
       weight: Number(item.weight_per_item) || 0,
       content: item.name ?? "",
       outbound_rf_1: item.sku ?? "",
-      package_type: "KT",
+      package_type: "PL",
       length_cm: length,
       width_cm: width,
       height_cm: height,
